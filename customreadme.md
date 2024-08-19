414b51c7b51557b8ec56ca056f382e0c87b2c8e5
Andrea Benini <andrea.benini@kettydo.com>
2024-08-20 00:53:33 +0200

    test

--- customreadme.md
+++ customreadme.md
@@ -1,7 +1,7 @@
 # Link utili
 
 * Preview sito su github https://zibibbalibbo.github.io/tenax/src/index.html
-* Login su Cognito https://tenax.auth.us-east-1.amazoncognito.com/login?client_id=2cso17vr764m158rcukqvnvm7d&response_type=code&scope=email&redirect_uri=https%3A%2F%2Fzibibbalibbo.github.io%2Ftenax%2Fsrc%2Fprofile.html
+* Login su Cognito https://tenax.auth.us-east-1.amazoncognito.com/login?client_id=2cso17vr764m158rcukqvnvm7d&response_type=code&scope=email&redirect_uri=https%3A%2F%2Fzibibbalibbo.github.io%2Ftenax%2Fdist%2Fprofile.html
 * API
 * * generica https://aw53pvvt09.execute-api.us-east-1.amazonaws.com/Prod
 * * endpoint https://aw53pvvt09.execute-api.us-east-1.amazonaws.com/Prod/pets 
--- src/js/components/loginChecker.js
+++ src/js/components/loginChecker.js
@@ -16,7 +16,7 @@ export function LoginChecker() {
         url += "?client_id=2cso17vr764m158rcukqvnvm7d";
         url += "&grant_type=authorization_code";
         url += "&code="+searchParams.get('code');
-        url += "&redirect_uri=https://zibibbalibbo.github.io/tenax/src/profile.html";
+        url += "&redirect_uri=https://zibibbalibbo.github.io/tenax/dist/profile.html";
 
         fetch(url, requestOptions)
             .then((result) => console.log(result))
@@ -26,7 +26,7 @@ export function LoginChecker() {
     return <div className="cta-container">
         <h1>Unisciti a Noi!</h1>
         <p>Scopri di più e inizia la tua avventura con noi oggi stesso.</p>
-        <a href="https://tenax.auth.eu-west-2.amazoncognito.com/login?client_id=34vorsr3caj9bni4thslmm2hse&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fzibibbalibbo.github.io%2Ftenax%2Fsrc%2Fprofile.html">Iscriviti
+        <a href="https://tenax.auth.eu-west-2.amazoncognito.com/login?client_id=34vorsr3caj9bni4thslmm2hse&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fzibibbalibbo.github.io%2Ftenax%2Fdist%2Fprofile.html">Iscriviti
             Ora</a>
     </div>
 }
